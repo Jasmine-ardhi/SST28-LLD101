eaf3f860603a1dda5b10bb5c1dde0a49f79d8363
Jasmine-ardhi <ardhijasmine28@gmail.com>
2026-03-02 09:11:23 +0530

    Refactored 9 and 10

---
+++ SOLID/Ex10/src/DefaultPricingService.java
@@ -0,0 +1,7 @@
+public class DefaultPricingService implements PricingService {
+    @Override
+    public double calculateFare(double km) {
+        double fare = 50.0 + km * 6.6666666667;
+        return Math.round(fare * 100.0) / 100.0;
+    }
+}
--- SOLID/Ex9/src/Main.java
+++ SOLID/Ex9/src/Main.java
@@ -1,7 +1,28 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("=== Evaluation Pipeline ===");
+//         Submission sub = new Submission("23BCS1007", "public class A{}", "A.java");
+//         new EvaluationPipeline().evaluate(sub);
+//     }
+// }
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Evaluation Pipeline ===");
-        Submission sub = new Submission("23BCS1007", "public class A{}", "A.java");
-        new EvaluationPipeline().evaluate(sub);
+
+        Submission sub = new Submission(
+                "23BCS1007",
+                "public class A{}",
+                "A.java");
+
+        Rubric rubric = new Rubric();
+
+        PlagiarismService plagiarism = new PlagiarismChecker();
+        GradingService grading = new CodeGrader(rubric);
+        ReportService report = new ReportWriter();
+
+        EvaluationPipeline pipeline =
+                new EvaluationPipeline(plagiarism, grading, report);
+
+        pipeline.evaluate(sub);
     }
 }
